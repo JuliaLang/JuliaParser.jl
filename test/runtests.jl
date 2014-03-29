c91e4aeb204a0106ca870f1b10c51ab40323665a
Jake Bolewski <jakebolewski@gmail.com>
2014-03-29 18:27:17 -0400

    add skip_to_eolgit --git-dir=/home/jake/.julia/JuliaParser/.git add test/runtests.jl tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,27 @@
 using JuliaParser
 using Base.Test  
 
-# write your own tests here
-@test 1 == 1
+const Lexer = JuliaParser.Lexer
+
+#= test skip to end of line =#
+
+io = IOBuffer("abcd\nabcd\n")
+Lexer.skip_to_eol!(io)
+@test position(io) == 5
+skip(io, 2)
+@test position(io) == 7
+Lexer.skip_to_eol!(io)
+@test position(io) == 10 
+@test eof(io) == true
+
+# no line break in buffer 
+io = IOBuffer("abcde")
+Lexer.skip_to_eol!(io)
+@test position(io) == 5
+@test eof(io) == true
+
+# empty buffer 
+io = IOBuffer()
+Lexer.skip_to_eol!(io)
+@test eof(io) == true
+
