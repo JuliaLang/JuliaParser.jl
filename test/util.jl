0fa1220c34fe92cdc622d8fbcdbaa9163cf3833e
Jake Bolewski <jakebolewski@gmail.com>
2014-06-03 17:15:50 -0400

    forgot to add color printing code

---
+++ test/util.jl
@@ -0,0 +1,10 @@
+const RED     = "\x1b[31m"
+const GREEN   = "\x1b[32m"
+const BOLD    = "\x1b[1m"
+const DEFAULT = "\x1b[0m"
+
+colored(s::String, color) = string(color, s, DEFAULT)
+
+red(s::String)   = colored(s, RED)
+green(s::String) = colored(s, GREEN)
+bold(s::String)  = colored(s, BOLD) 
