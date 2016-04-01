eb253d396af4714623a0b0f5c0f8cee0c3cd3bfe
Keno Fischer <kfischer@college.harvard.edu>
2016-04-01 07:10:18 +0000

    Fix repl add-in

--- bin/repl.jl
+++ bin/repl.jl
@@ -38,7 +38,7 @@ function Core.include(fname::ByteString)
             !isa(e, Main.JuliaParser.Diagnostics.Diagnostic) && rethrow(e)
             rethrow(REPLDiagnostic(fname, file, e))
         end
-        result = ccall(:jl_toplevel_eval, Any, (Any), ¬ast)
+        result = ccall(:jl_toplevel_eval, Any, (Any,), ¬ast)
     end
     result
 end
