d5f3ed8fd53b5bf88a82cdae914e75be8ebd1dd2
David Anthoff <anthoff@berkeley.edu>
2017-01-12 14:28:43 -0800

    Make code agnostic with respect to the package directory

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,7 +1,7 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
 
-    code = readstring(Pkg.dir("JuliaParser","src","parser.jl"))
+    code = readstring(joinpath(dirname(@__FILE__), "parser.jl"))
     buf = IOBuffer(code)
     ts = Lexer.TokenStream{Tokens.Token}(buf)
     while !Lexer.eof(ts); Parser.parse(ts); end
