b8a5daeba615866d3d1875d59f56e3a8df687c69
Alex Arslan <ararslan@comcast.net>
2017-01-15 13:20:31 -0800

    Remove unnecessary copy in test

--- test/LineNumbers.jl
+++ test/LineNumbers.jl
@@ -4,7 +4,7 @@ using Base.Test
 
 code = "f() = 1 + 2 - 3\n"
 @test SourceFile(code)[1:2] == Vector{UInt8}[Vector{UInt8}("f() = 1 + 2 - 3"), Vector{UInt8}("")]
-data2 = Vector{UInt8}[copy(Vector{UInt8}(code)),[]]
+data2 = Vector{UInt8}[Vector{UInt8}(code),[]]
 LineBreaking(UInt64(1),SourceFile(code),data2)[7] = 'x'
 @test String(data2[1]) == "f() = x + 2 - 3\n"
 @test length(SourceFile("f() = 1 + 2 - 3")) == 1
